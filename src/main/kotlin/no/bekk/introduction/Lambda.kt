46203b2d8d31bbc1284d6935e46dd00dd4e84c17
gauteab <gaute.berge@gmail.com>
2023-07-24 16:50:46 +0200

    lambda intro

---
+++ src/main/kotlin/no/bekk/introduction/Lambda.kt
@@ -0,0 +1,24 @@
+package no.bekk.introduction
+
+fun double(number: Int): Int {
+    return number * 2
+}
+
+// Oppgave: Skriv om
+fun maybeDouble(number: Int?): Int? {
+    return if (number != null) {
+        double(number)
+    } else {
+        null
+    }
+}
+
+fun main() {
+    val number = 2
+    val doubledNumber = double(number)
+    println(doubledNumber)
+
+    val maybeNumber: Int? = null
+    val maybeDoubledNumber = maybeDouble(maybeNumber)
+    println(maybeDoubledNumber)
+}
